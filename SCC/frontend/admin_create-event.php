8bd8934e4ad6a2a4be423e3d8c24bcfedb21fc71
Ivan Garzon <ivan.garzon@amilia.com>
2019-11-27 10:41:42 -0500

    Added trigger queries in PHP
    The trigger queries to insert new rows in related tables was rewritten in PHP.
    Created new file to get all users from DB and generate a select list
    Updated create event form

--- SCC/frontend/admin_create-event.php
+++ SCC/frontend/admin_create-event.php
@@ -1,21 +1,32 @@
 <?php
 	require("navbar.php");
+	require("../backend/users/get_users.php");
 ?>
 
+<!doctype html>
+<html class="no-js" lang="">
+
 <head>
-	<title> p1 -index </title>
-	<style>
+  <meta charset="utf-8">
+  <title> Create Event </title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#fafafa">
+  <style>
 		a{
 			margin-right:20px;
 		}
-	</style>
+  </style>
 </head>
 
 <body>	
 	<h1>Admin View</h1>
 	<form action="../backend/eventMaker.php" method="POST">
-		<p>Enter user id</p>
-		<input type="text" name="user_id">
+		<p>Enter event name</p>
+		<input type="text" name="event_name">
+		<?php if(!empty($users) && sizeof($users) != 0) { echo "<p>Select an event manager</p>$select_list";}
+		      else echo "<p>Write the user id of the event manager</p><input type='text' name='user_id' required>";
+		?>
 		<p>Select event type</p>
 		<select name="event_type">
 			<option value="non-profit_recurrent">non-profit recurrent</option>
@@ -24,6 +35,10 @@
 			<option value="private">private non-recurrent</option>
 		</select>
 		<p>Enter user fee</p>
-		<input type="text" name="event_fee">
+		<input type="text" name="event_fee"><br><br>
 		<input type="submit" value="Create new Event" name="submit">
 	</form>
+
+</body>
+
+</html>
