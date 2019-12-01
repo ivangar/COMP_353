8b4497e6c14f083596e0917e65f43f74a22d2c6f
NicoChampagne <nicolas.b.champagne@gmail.com>
2019-12-01 01:54:21 -0500

    Working inbox and send emails

--- SCC/backend/emails.php
+++ SCC/backend/emails.php
@@ -1,9 +1,14 @@
 <?php	
 
-function getEmails($userEmails){
+function getEmails(){
+    if(!isset($_SESSION)){
+        session_start();
+    }
     include ("connection.php");
 
-	$sql = "SELECT * from emails Where user_email = $userEmails";
+    $user_id = $_SESSION['active_user']['user_id'];
+
+	$sql = "SELECT * from emails Where receiver_id = $user_id";
 	$results = $conn->query($sql);
 	
 	if($results != true)
--- SCC/frontend/emails_view.php
+++ SCC/frontend/emails_view.php
@@ -1,53 +1,83 @@
-<html>
-    <head>
+<body>
         <script>
             function alertFunction(sender, subject, date, emailContent)
             {
-            alert("From: " + sender + '\nSubject: ' + subject + "\n" + emailContent + "\n\n\nSent on: " + date);
+                alert("From: " + sender + '\nSubject: ' + subject + "\n" + emailContent + "\n\n\nSent on: " + date);
             }
         </script>
-    </head>
-
-    <body>
         <?php
-            include ("emails.php");
-            $emails = getEmails($_SESSION['active_user']['email']);
+            include ("../backend/emails.php");
+            $emails = getEmails();
 
-            if($emails.count == 0)
+            echo
+                "
+                <h2>Email Page</h2>
+                <hr />
+                <h4>Inbox</h4>
+                ";
+            if($emails->num_rows == 0)
             {
                 echo "Inbox is empty";
-            } 
+            }
             else
             {
-                echo "<table width='600'><tr><th>Sender</th><th>Subject</th><th>Date</th><th>See email</th></tr>";
+                echo "<table width='600' border='1'><tr><th>Sender</th><th>Subject</th><th>Date</th><th>See email</th></tr>";
 
-                foreach($emails as $email)
+                for ($x = 0; $x < $emails->num_rows; $x++)
                 {
+                    $email = $emails->fetch_assoc();
+
                     echo "<tr><td>";
-                    echo $email.sender;
+                    echo $email["sender"];
                     echo "</td>";
 
                     echo "<td>";
-                    echo $email.title;
+                    echo $email["title"];
                     echo "</td>";
 
                     echo "<td>";
-                    echo $email.date;
+                    echo $email["date"];
                     echo "</td>";
 
-                    echo "<td><input type='button' onclick='alertFunction(";
-                    echo $email.sender;
-                    echo",";
-                    echo $email.title;
-                    echo ",";
-                    echo $email.date;
-                    echo ",";
-                    echo $email.body;
-                    echo ")' value='Show Email' /></td></tr>";
+                    echo '<td><input type="button" onclick=\'alertFunction(';
+                    echo "\"".$email["sender"];
+                    echo "\",\"";
+                    echo $email["title"];
+                    echo "\",\"";
+                    echo $email["date"];
+                    echo "\",\"";
+                    echo $email["body"]."\"";
+                    echo ')\' value=\'Show Email\' /></td></tr>';
                 }
 
                 echo "</table>";
             }
+
+
+
+        echo
+        "
+            <br />
+            <h4>Send an Email</h4>
+            <hr />
+            <table width='600' >
+            <form action='../backend/send_email.php' method='post' enctype='multipart/form-data'>
+            <tr>
+            <td width='20%'>To: <input type='text' name='email_receiver' id='email_receiver'/></td>
+            </tr>
+            <tr>
+            <td width='20%'>Subject: <input type='text' name='email_title' id='email_title'/></td>
+            </tr>
+            <tr>
+            <td width='20%'>Enter email contents here <br /><textarea name='email_body' id='email_body' rows='5' cols='30' ></textarea></td>
+            </tr>
+        
+            <tr>
+            <td><input type='submit' name='submit' /></td>
+            </tr>
+        
+            </form>
+            </table>";
         ?>
     </body>
 
