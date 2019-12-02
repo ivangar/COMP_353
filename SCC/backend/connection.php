c31b1317526b1227de6c740f15243624ef8423e7
Ragith Sabapathipillai <r_sabapa@live.concordia.ca>
2019-12-01 23:34:02 -0500

    add back connection.php

---
+++ SCC/backend/connection.php
@@ -0,0 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "scc_db";
+
+
+
+// Creating connection
+$conn = new mysqli($servername, $username, $password);
+
+// Checking connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+//For now I commented these, cauz it was printing the successful message every time
+/*else{
+    echo "Connected successfully";
+}*/
+$conn->select_db('scc_db');
+?>
