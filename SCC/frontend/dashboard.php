fd1dfbe53e359ecec86f7dd0cf1b8d475cbe6353
Ivan Garzon <garzon861@gmail.com>
2019-12-05 16:16:23 -0500

    Removed dashboard message

--- SCC/frontend/dashboard.php
+++ SCC/frontend/dashboard.php
@@ -3,7 +3,6 @@ include("navbar.php");
 include("../backend/user_role_relation.php");
 include("event_manager_page.php");
 if(isset($_SESSION['new_event'])) {
-    echo "New event created!";
     unset($_SESSION['new_event']);
 }
 ?>
