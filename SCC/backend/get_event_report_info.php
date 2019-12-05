bb6496e6ddba9d9548120fa1e0bdbd115fe1ac65
Siamak Samie <siamak.samie@gmail.com>
2019-12-05 16:43:08 -0500

    paint report

--- SCC/backend/get_event_report_info.php
+++ SCC/backend/get_event_report_info.php
@@ -1,31 +1,25 @@
 <?php
-include("connection.php");
-session_start();
-if(!isset($_SESSION['active_user']))
-{
-	header("Location: ../frontend/dashboard.php");
+include "connection.php";
+// session_start();
+if (!isset($_SESSION['active_user'])) {
+    header("Location: ../frontend/dashboard.php");
 }
-include ("connection.php");
-$event_id =  $_GET["event_id"];
+include "connection.php";
+$event_id = $_GET["event_id"];
 $user_id = $_SESSION['active_user']['user_id'];
 
 $sql = "SELECT * FROM events WHERE event_manager_id = $user_id AND event_id = $event_id";
 $result = $conn->query($sql);
 
- if ($result->num_rows == 0) {
-      //header("Location: ../frontend/dashboard.php");
-	echo "user : " . $user_id . " event : ". $event_id;
- }
+if ($result->num_rows == 0) {
+    //header("Location: ../frontend/dashboard.php");
+    echo "user : " . $user_id . " event : " . $event_id;
+}
 
 $event = $result->fetch_assoc();
-$event_id =  $event["event_id"];
+$event_id = $event["event_id"];
 $event_name = $event["event_name"];
 $event_cost = $event["total_cost"];
 $event_start = $event["start_date"];
 $event_end = $event["end_date"];
 $event_primary_group_id = $event["primary_event_group_id"];
-
-
-
-
-?>
