be6a371f3dd971ae35c9850e320002a213e635c8
Ivan Garzon <ivan.garzon@amilia.com>
2019-12-10 20:43:09 -0500

    Fix Set Resources page
    - Added a new file to update the resources table in the DB
    - Added css style

---
+++ SCC/backend/update_resource.php
@@ -0,0 +1,49 @@
+<?php
+/*
+* This file is used to display event details of a specific event ID.
+* It should be called from another file, by passing the event id as part of the url=event_details.php?event_id=2
+*/
+
+ini_set("display_errors","1");
+session_start();
+require("connection.php");
+
+if(isset($_POST) && (!empty($_POST))){
+
+  //Setting up post params
+
+  $data = $_POST;
+  $resource_id = $data['resource_id'];
+  $flat_fee = $data['flat_fee']; 
+  $resource_type = $data["resource_type"];
+  $unit = $data["unit"];
+  $resource_name = $data['resource_name'];
+  $extra_fee = $data['extra_fee']; 
+  $discount = $data['discount']; 
+  $payment_receiver = $data['payment_receiver']; 
+  
+  //Test Null values, set default for empty fields and wrap strings around single quotes
+
+  $flat_fee = !empty($flat_fee) ? "$flat_fee" : "NULL";
+  $resource_type = !empty($resource_type) ? "'$resource_type'" : "NULL"; 
+  $unit = !empty($unit) ? "'$unit'" : "NULL"; 
+  $resource_name = !empty($resource_name) ? "'$resource_name'" : "NULL"; 
+  $extra_fee = !empty($extra_fee) ? "$extra_fee" : "NULL";
+  $discount = !empty($discount) ? "$discount" : "NULL";
+  $payment_receiver = !empty($payment_receiver) ? "'$payment_receiver'" : "NULL";
+
+    
+  //Update resources table
+  $sql = "UPDATE resources 
+          SET flat_fee = $flat_fee, type = $resource_type, unit = $unit, resource_name = $resource_name, extra_fee = $extra_fee, discount = $discount, payment_receiver = $payment_receiver
+          WHERE resource_id = $resource_id";
+  
+    if ($conn->query($sql) === TRUE) {
+        echo "updated";
+    } else {
+        echo "error " . $conn->error;
+    }
+
+      
+
+}
